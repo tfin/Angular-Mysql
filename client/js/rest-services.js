ecff600dce08b0021e1520b22e683b06298c6e50
tfin <terencefin@gmail.com>
2014-04-15 22:16:33 -0600

    pointing home

--- client/js/rest-services.js
+++ client/js/rest-services.js
@@ -3,10 +3,12 @@
 angular.module('myApp.restServices', ['ngResource'])
     .factory('Employee', ['$resource',
         function ($resource) {
-            return $resource('http://localhost:3000/employees/:employeeId', {});
+            //return $resource('http://localhost:3000/employees/:employeeId', {});
+             return $resource('http://192.168.0.4:3000/employees/:employeeId', {});
         }])
 
     .factory('Report', ['$resource',
         function ($resource) {
-            return $resource('http://localhost:3000/employees/:employeeId/reports', {});
+            //return $resource('http://localhost:3000/employees/:employeeId/reports', {});
+             return $resource('http://192.168.0.4:3000/employees/:employeeId/reports', {});
         }]);
